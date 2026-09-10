9a0290e5c212b88acd1f42096caa99d7cc2bb83a
alice <pubiwiza@mail.ru>
2026-09-10 15:17:35 +0500

    Task2

--- Tyuiu.GusevaAS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.GusevaAS.Sprint0.Task1.V0/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tyuiu.SpirinIS.Sprint0.Task1.V0
+namespace Tyuiu.GusevaAS.Sprint0.Task1.V0
 {
     class Program
     {
---
+++ Tyuiu.GusevaAS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.GusevaAS.Sprint0.Task2.V0.Test
+{
+    using Tyuiu.GusevaAS.Sprint0.Task2.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алиса";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Алиса", res);
+        }
+    }
+}
