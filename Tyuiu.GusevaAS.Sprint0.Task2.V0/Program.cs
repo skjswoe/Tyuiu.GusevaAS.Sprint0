ae007c7c5bae4bb393615965b229d6741124d83b
alice <pubiwiza@mail.ru>
2026-09-14 14:08:59 +0500

    Task3

--- Tyuiu.GusevaAS.Sprint0.Task2.V0/Program.cs
+++ Tyuiu.GusevaAS.Sprint0.Task2.V0/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Tyuiu.GusevaAS.Sprint0.Task2.V0.Lib;
+﻿using Tyuiu.GusevaAS.Sprint0.Task2.V0.Lib;
 
 namespace Tyuiu.GusevaAS.Sprint0.Task2.V0
 {
---
+++ Tyuiu.GusevaAS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.GusevaAS.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.GusevaAS.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(6, 7));
+            Console.ReadKey();
+        }
+    }
+}
