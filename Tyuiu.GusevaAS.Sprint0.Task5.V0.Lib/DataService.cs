f14a0fe2526a1022334eaf50caec3d9413b0fece
alice <pubiwiza@mail.ru>
2026-09-14 15:12:06 +0500

    Task5

---
+++ Tyuiu.GusevaAS.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tyuiu.GusevaAS.Sprint0.Task5.V0.Lib
+{
+    public static class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1.0;
+            }
+            else
+            {
+                return (double)a / b;
+            }
+        }
+    }
+}
